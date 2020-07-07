597eecbcd5215236e59c4cbe1dae321c4b745961
morutole <50069930+morutole@users.noreply.github.com>
2020-07-07 14:08:55 +0900

    一時条件変更

--- 77simu/cpp_estimator_a/local_library.h
+++ 77simu/cpp_estimator_a/local_library.h
@@ -24,13 +24,13 @@ const double propagation_step_time = 1e-3; //1step当たりの時間をいくつ
 const int log_period = 10; //何stepごとにログをとるか propagation_step_time * log_periodで何sごとにログが出るか決まる。
 const int GPS_period = 1; //何sごとにGPSから[r,v]に関する情報を渡すか。
 
-const double position_error = 0.5; //位置に乗る観測誤差[m]
-const double velocity_error = 5e-2; //速度に乗る観測誤差[m/s];
+const double position_error = 6e-2; //位置に乗る観測誤差[m]
+const double velocity_error = 6e-3; //速度に乗る観測誤差[m/s];
 
-const double position_noise = 1.0; //位置に乗るノイズ[m]
-const double velocity_noise = 0.1; //速度に乗るノイズ[m/s]
-const double acceleration_noise = 1e-2; //加速度に乗るノイズ[m/s^2] TBD
-const double Cd_noise = 1e-13; //[/m]
+const double position_noise = 1e-1; //位置に乗るノイズ[m]
+const double velocity_noise = 1e-2; //速度に乗るノイズ[m/s]
+const double acceleration_noise = 1e-3; //加速度に乗るノイズ[m/s^2] TBD
+const double Cd_noise = 1e-11; //[/m]
 
 const double initial_estimate_airdrag_force = 1.3e-5; //[N]
 extern double Cd_error; //[/m] 普通のCdだけでなく、rho*Cd*A/m全体としてのCd;
